708ac00de09d4241c388fa82f52d4ceee3f70d0c
j-huang-rj <100328343+j-huang-rj@users.noreply.github.com>
2025-05-25 22:27:57 +0800

    Add theming support

--- utils.py
+++ utils.py
@@ -1,21 +1,21 @@
 import time
 
 
-def paper_to_slide(pdf_path: str, pptx_path: str) -> bool:
-
+def paper_to_slide(pdf_path, output_pptx_path, theme_pptx_path):
     try:
-        # Read pdf from pdf_path
+        # Read pdf and theme from pdf_path and theme_pptx_path
         # ...
 
-        # Simulate processing
-        time.sleep(5)
+        # Simulate processing logic
+        time.sleep(15)
 
         # Output pptx to pptx_path
-        with open(pptx_path, "w") as f:
+        with open(output_pptx_path, "w") as f:
             f.write(f"This is a dummy PPTX.")
 
         return True
 
     except Exception as e:
         print(f"An error occurred during conversion: {e}")
+        
         return False
